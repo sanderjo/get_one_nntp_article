b511ecf43b87a76d16751d7b4c8dacf7d027ad9d
Sander <sander.jonkers+github@gmail.com>
2022-11-08 15:52:21 +0100

    Create get_nntp_welcome.py

---
+++ get_nntp_welcome.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+
+# Get one article from a NNTP / NNTPS server
+
+import nntplib
+import sys
+
+try:
+        mynntpserver = sys.argv[1]
+except:
+        print("Usage:", sys.argv[0], " nntpserver")
+        sys.exit(-1)
+
+
+print(mynntpserver, end=': ' )
+
+try:
+        s = nntplib.NNTP_SSL(mynntpserver, port=563)
+        print(s.getwelcome())
+except:
+        print("error")
+
+try:
+        s.quit()
+except:
+        pass
